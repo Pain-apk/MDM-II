4ba89271450957af30d7c02a18e8ff8699ae64bb
Aditya <adi465996@gmail.com>
2026-01-21 09:25:11 +0530

    updated shop.jsx

--- MDM/src/pages/shop.jsx
+++ MDM/src/pages/shop.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 export default function Shop() {
+    const Heading ={
+        color: "blue",
+        fontSize: "48px",
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: "20px"
+    }
   return (
-    <div style = {{ color: "blue" }}>Shop</div>
+    <div style = {Heading}><h1>Shop</h1></div>
 
   );
 }
